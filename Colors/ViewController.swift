75aea135eda731b91c6ac0df47622bb206f7caa0
Allison Dolan <allisondolan@ATM0561828DYN.dhcp.missouri.edu>
2018-09-27 12:19:04 -0500

    First Commit

---
+++ Colors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Colors
+//
+//  Created by Allison Dolan on 9/27/18.
+//  Copyright © 2018 Allison Dolan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
